a8447a217fd96ed7279c0e5bae40f43cb3f954f5
robC_vW <robiecalong2@gmail.com>
2025-06-14 11:50:50 -0500

    render Puppy data by onClick event

--- puppyPals-react/src/App.jsx
+++ puppyPals-react/src/App.jsx
@@ -4,15 +4,36 @@ import { useState } from "react";
 
 function App() {
   const [puppies, setPuppies] = useState(puppyList);
+  const [featPupId, setFeatPupId] = useState(null);
 
   console.log(puppies);
+
+  function handleClick(id) {
+    console.log(id);
+    setFeatPupId(id);
+  }
+
+  const featuredPup = puppies.find((puppy) => puppy.id === featPupId);
+  console.log(featuredPup);
 
   return (
     <div>
       <h1>this is a test</h1>
-
+      {featPupId && (
+        <div>
+          <h2>{featuredPup.name}</h2>
+          <ul>
+            <li>Age: {featuredPup.age}</li>
+            <li>Email: {featuredPup.email}</li>
+          </ul>
+        </div>
+      )}
       {puppies.map((puppy) => {
-        return <p key={puppy.name}>{puppy.name}</p>;
+        return (
+          <p onClick={() => handleClick(puppy.id)} key={puppy.id}>
+            {puppy.name}
+          </p>
+        );
       })}
     </div>
   );
