c63249943078b39c0cecb2fbbb836cba00c8c582
robC_vW <robiecalong2@gmail.com>
2025-06-14 09:01:47 -0500

    added data and useState

--- puppyPals-react/src/App.jsx
+++ puppyPals-react/src/App.jsx
@@ -1,12 +1,21 @@
-import './App.css'
+import "./App.css";
+import { puppyList } from "./data.js";
+import { useState } from "react";
 
 function App() {
+  const [puppies, setPuppies] = useState(puppyList);
+
+  console.log(puppies);
 
   return (
-    <>
+    <div>
+      <h1>this is a test</h1>
 
-    </>
-  )
+      {puppies.map((puppy) => {
+        return <p key={puppy.name}>{puppy.name}</p>;
+      })}
+    </div>
+  );
 }
 
-export default App
+export default App;
